73c35853354d887383a0a9e3f8e3d67dc26882f3
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2025-01-10 20:54:16 -0500

    Create simple server

--- plc/index.ts
+++ plc/index.ts
@@ -1,60 +1,17 @@
-import { PrismaClient, User } from "@prisma/client";
-import { PlcAgent, PlcRecord } from "./plcAgent";
+import http from "http";
+const port = process.env.PORT || 3000;
+import { backfill } from "./backfill";
 
-const prisma = new PrismaClient();
+// Start process that keeps PLC directory up-to-date
+backfill();
 
-const LATEST_RECORD_SETTING_KEY = "latestRecord";
-
-async function addHandles(records: PlcRecord[], latestRecord: Date) {
-  if (records.length) {
-    await prisma.user.createMany({
-      data: records.map((record) => {
-        const handleParts = record.handle.split(".");
-        const handlePart1 = handleParts.pop() || "";
-        const handlePart2 = handleParts.pop() || "";
-        const handlePart3 = handleParts.pop() || "";
-        return {
-          ...record,
-          handlePart1,
-          handlePart2,
-          handlePart3,
-        };
-      }),
-    });
-  }
-  await prisma.setting.upsert({
-    where: { key: LATEST_RECORD_SETTING_KEY },
-    update: {
-      value: latestRecord.toISOString(),
-    },
-    create: {
-      key: LATEST_RECORD_SETTING_KEY,
-      value: latestRecord.toISOString(),
-    },
-  });
-}
-
-async function main() {
-  const latestRecord = await prisma.setting.findFirst({
-    where: { key: LATEST_RECORD_SETTING_KEY },
-  });
-
-  const plcAgent = new PlcAgent(new Date(latestRecord?.value || 0));
-
-  plcAgent.startExport((data) => {
-    console.log(data.latestRecord, data.records.length);
-    addHandles(data.records, data.latestRecord);
-  });
-}
-
-main();
-
-// Disconnect from DB on shutdown
-["SIGINT", "SIGTERM", "SIGQUIT"].forEach((signal) =>
-  process.on(signal, async () => {
-    console.log("Disconnecting from DB...");
-    await prisma.$disconnect();
-    console.log("Exiting proess...");
-    process.exit();
+// Simple HTTP server for API
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello world");
+    res.end();
   })
-);
+  .listen(port, () => {
+    console.log(`App running on port ${port}`);
+  });
