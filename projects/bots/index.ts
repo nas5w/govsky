4119ca39baea5c051e567b3bdf7ed94d9f58bad3
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2025-01-15 11:46:26 -0500

    Don't crash if bot fails

--- projects/bots/index.ts
+++ projects/bots/index.ts
@@ -4,7 +4,7 @@ import { GovskyBot } from "./govsky-bot";
 import { getUserForAllDomains } from "./helpers";
 import { govskyUsBot } from "./configs";
 
-const BOT_INTERVAL = 0.5 * 60_000;
+const BOT_INTERVAL = 5 * 60_000;
 
 dotenv.config();
 
@@ -15,20 +15,24 @@ async function runBots() {
   const readOnly = process.env.READ_ONLY === "true";
 
   for (const botConfig of bots) {
-    console.log(`Running bot: ${botConfig.name} (${botConfig.handle})`);
-    const bot = new GovskyBot(botConfig, readOnly);
+    try {
+      console.log(`Running bot: ${botConfig.name} (${botConfig.handle})`);
+      const bot = new GovskyBot(botConfig, readOnly);
 
-    console.log("Logging in...");
-    await bot.login();
+      console.log("Logging in...");
+      await bot.login();
 
-    console.log("Get relevant users...");
-    const users = await getUserForAllDomains(botConfig.domains);
+      console.log("Get relevant users...");
+      const users = await getUserForAllDomains(botConfig.domains);
 
-    console.log("Following and/or unfollowing users...");
-    await bot.followOrUnfollow(users);
+      console.log("Following and/or unfollowing users...");
+      await bot.followOrUnfollow(users);
 
-    console.log("Adding and/or removing users from lists...");
-    await bot.addOrRemoveFromLists(users);
+      console.log("Adding and/or removing users from lists...");
+      await bot.addOrRemoveFromLists(users);
+    } catch (e) {
+      console.error("Error running bot!", e);
+    }
   }
 }
 
